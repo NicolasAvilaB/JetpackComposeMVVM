9c7cf55fcbf430c5daf8b3570f9bb09f7b65dbcb
NicolasAvilaB <nicolasab1996@gmail.com>
2023-01-16 17:40:21 -0300

    Modificación LoginServices

--- app/src/main/java/com/example/jetpackcomposeinstagram/data/LoginService.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/data/LoginService.kt
@@ -1,15 +1,12 @@
 package com.example.jetpackcomposeinstagram.data
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class LoginService @Inject constructor(val retrofit : NetworkModule){
+class LoginService @Inject constructor(
+    val loginclient: LoginClient,
+) {
 
-    suspend fun doLogin():Any {
-        return withContext(Dispatchers.IO){
-           retrofit.provideLoginRetrofitService().create(LoginClient::class.java).doLogin()
-        }
-    }
+    suspend fun doLogin() { loginclient.doLogin() }
+    /*retrofit.provideLoginRetrofitService().create(LoginClient::class.java).doLogin()*/
 
 }
